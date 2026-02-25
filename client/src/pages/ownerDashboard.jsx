79c20c4941a6479d9f2c6e9051fe2d428ff63104
cgartco6 <155203611+cgartco6@users.noreply.github.com>
2026-02-25 15:52:50 +0200

    Create ownerDashboard.jsx

---
+++ client/src/pages/ownerDashboard.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import RevenueTracker from '../components/ownerDashboard/RevenueTracker';
+import TargetProgress from '../components/ownerDashboard/TargetProgress';
+import ImpulseBuyerMetrics from '../components/ownerDashboard/ImpulseBuyerMetrics';
+import AutoScaleIndicator from '../components/ownerDashboard/AutoScaleIndicator';
+
+const OwnerDashboard = () => {
+  return (
+    <div className="container" style={{ padding: '2rem 0' }}>
+      <h1>Owner Dashboard</h1>
+      <RevenueTracker />
+      <TargetProgress />
+      <ImpulseBuyerMetrics />
+      <AutoScaleIndicator />
+    </div>
+  );
+};
